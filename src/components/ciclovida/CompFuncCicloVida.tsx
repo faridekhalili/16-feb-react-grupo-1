0172a14cea1285eaca366350c8ba63413fe369fe
Faridekhalili <khalili.f@northeastern.edu>
2024-03-19 20:26:16 -0400

    Add useRef.

--- src/components/ciclovida/CompFuncCicloVida.tsx
+++ src/components/ciclovida/CompFuncCicloVida.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 const CompFuncCicloVida = () => {
-    const [miVariable, setMiVariable] = useState<boolean | undefined>()
+    const miVariable = useRef<boolean | undefined>()
     // 1 - Componente se monta
     // 2 - Componente se actualiza
     // 3 - Componente se va a desmontar
@@ -11,16 +11,12 @@ const CompFuncCicloVida = () => {
             console.log("Se va a desmontar")
         }
     }, [])
-    useEffect(() => {
-        if (typeof miVariable === "undefined") return
-        console.log("miVariable ha cambiado")
-    }, [miVariable])
 
     const updateVar = () => {
-        setMiVariable((val) => {
-            if (typeof val === "undefined") return true
-            return !val
-        })
+        miVariable.current =
+            typeof miVariable.current === "undefined"
+                ? true
+                : !miVariable.current
     }
 
     return (
